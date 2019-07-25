12471545baa2655d89c2265938e7b02b0f0ec3e3
Amit Paul <amit.paul0025@gmail.com>
2019-07-25 13:52:42 +0530

    full structure for kumori request model,views, security

--- models/models.py
+++ models/models.py
@@ -2,14 +2,22 @@
 
 from odoo import models, fields, api
 
-# class kumori_demo(models.Model):
-#     _name = 'kumori_demo.kumori_demo'
+class kumoriRequest(models.Model):
+    _name = 'kumori.request'
+    _description = 'Kumori Request'
 
-#     name = fields.Char()
-#     value = fields.Integer()
-#     value2 = fields.Float(compute="_value_pc", store=True)
-#     description = fields.Text()
-#
-#     @api.depends('value')
-#     def _value_pc(self):
-#         self.value2 = float(self.value) / 100
+    name = fields.Char(string="Request", required="true")
+    date_from = fields.Datetime(string="Starting Date", default=fields.Datetime.now())
+    date_to = fields.Datetime(string="End Date", required="false")
+    employee_id = fields.Many2one(comodel_name="hr.employee", string="Employee", required="true")
+    car_id = fields.Many2one(comodel_name="fleet.vehicle", string="Car", required="true")
+    
+
+
+    # value = fields.Integer()
+    # value2 = fields.Float(compute="_value_pc", store=True)
+    # description = fields.Text()
+
+    # @api.depends('value')
+    # def _value_pc(self):
+    #     self.value2 = float(self.value) / 100
